2d714112591f8c73837810e8d2fbb12e300965fb
DMBabich <44494405+DMBabich@users.noreply.github.com>
2020-05-02 00:17:30 +0300

    Add files via upload

---
+++ my_small_xml_parser/xml_parser_full.py
@@ -0,0 +1,35 @@
+from lxml import etree as et
+
+#Выбираем файл и получаем дерево
+tree = et.parse('mebel.xml')
+root = tree.getroot()
+#Создаем файл для записи инфрмации и записываем корневой тег
+file_output = open('TEST___0001.txt', 'w')
+file_output.writelines('##############\n')
+file_output.writelines(root.tag + '\n')
+file_output.writelines('===============\n')
+
+#Обрабатываем первый уровень дерева
+for item in root:
+    if item.tag == '{urn:1C.ru:commerceml_2}Каталог':
+        new = item
+
+#Обрабатываем второй путь дерева
+for item in new:
+    if item.tag == '{urn:1C.ru:commerceml_2}Товары':
+        child = item
+
+#После нахождения уровня "Товар" ищем в нем необходимую информацию
+#И записываем полученный результат в файл
+for item in child:
+    for alfa in item:
+        if alfa.tag == '{urn:1C.ru:commerceml_2}Наименование':
+            file_output.writelines('\n')
+            file_output.writelines('\n')
+            stroka = str('name:\t' + str(alfa.text) + str(alfa.attrib) + '\n')
+            file_output.writelines(stroka)
+        if alfa.tag == '{urn:1C.ru:commerceml_2}Картинка':
+            stroka = str('path:\t' + str(alfa.text) + str(alfa.attrib) + '\n')
+            file_output.writelines(stroka)
+
+file_output.close()
