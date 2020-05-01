81cc022f181ae46f0b9d50ecff64650c291717fd
DMBabich <44494405+DMBabich@users.noreply.github.com>
2020-05-02 00:19:08 +0300

    Update xml_parser_full.py

--- my_small_xml_parser/xml_parser_full.py
+++ my_small_xml_parser/xml_parser_full.py
@@ -1,26 +1,26 @@
 from lxml import etree as et
 
-#Выбираем файл и получаем дерево
+# Выбираем файл и получаем дерево
 tree = et.parse('mebel.xml')
 root = tree.getroot()
-#Создаем файл для записи инфрмации и записываем корневой тег
+# Создаем файл для записи инфрмации и записываем корневой тег
 file_output = open('TEST___0001.txt', 'w')
 file_output.writelines('##############\n')
 file_output.writelines(root.tag + '\n')
 file_output.writelines('===============\n')
 
-#Обрабатываем первый уровень дерева
+# Обрабатываем первый уровень дерева
 for item in root:
     if item.tag == '{urn:1C.ru:commerceml_2}Каталог':
         new = item
 
-#Обрабатываем второй путь дерева
+# Обрабатываем второй путь дерева
 for item in new:
     if item.tag == '{urn:1C.ru:commerceml_2}Товары':
         child = item
 
-#После нахождения уровня "Товар" ищем в нем необходимую информацию
-#И записываем полученный результат в файл
+# После нахождения уровня "Товар" ищем в нем необходимую информацию
+# И записываем полученный результат в файл
 for item in child:
     for alfa in item:
         if alfa.tag == '{urn:1C.ru:commerceml_2}Наименование':
